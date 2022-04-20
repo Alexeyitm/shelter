13ac765c3f7e4a89054feb3b00fef50cab66be18
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-04-20 16:46:53 +0500

    feat: feature scripts

---
+++ shelter/pages/main/scripts/variables.js
@@ -0,0 +1,16 @@
+export const page = document.querySelector('.page');
+
+export const petCard = document.querySelectorAll('.our-friends__card');
+export const closePopupButton = document.querySelector('.popup__button-close');
+export const popup = document.querySelector('.popup');
+export const popupOverlay = document.querySelector('.popup__overlay');
+export const popupContainer = document.querySelector('.popup__container');
+
+export const leftButton = document.querySelector('.our-friends__last');
+export const rightButton = document.querySelector('.our-friends__next');
+
+export const burgerButton = document.querySelector('.button-burger');
+export const burgerLinks = document.querySelectorAll('.burger__link');
+export const burgerPopup = document.querySelector('.burger');
+export const burgerMenu = document.querySelector('.burger__menu');
+export const headerLogo = document.querySelector('.header__logo');
